c3faa7370d3140019be0bf86af7a3af4913c2843
Michael <mtse431@gmail.com>
2022-08-22 10:58:15 +0700

    Display Only The First Dropdown

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 //Dropdown Animation
+$(".hidden").hide();
 $(".btn-group").click(function (event) {
     let btnTarget = event.currentTarget.id;
     $(`#${btnTarget} + .guide__card-container`).slideToggle();
