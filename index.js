93f8814a76c58d08a7985679b4abd119751a0182
Michael <mtse431@gmail.com>
2022-08-22 10:11:41 +0700

    Create Dropdown Animation to Guide Section on Subscribe Page

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-
+//Dropdown Animation
+$(".btn-group").click(function (event) {
+    let btnTarget = event.currentTarget.id;
+    $(`#${btnTarget} + .guide__card-container`).slideToggle();
+    $(`#${btnTarget} .guide__dropdown-arrow`).toggleClass("down");
+});
